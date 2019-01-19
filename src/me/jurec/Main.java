40a39a33f84a494f143aad8bb78dcbb6ed05e735
Michal Polakiewicz <michal.polakiewicz@hotmail.com>
2019-01-19 14:40:38 +0100

    multiply

--- src/me/jurec/Main.java
+++ src/me/jurec/Main.java
@@ -15,8 +15,8 @@ public class Main {
         return 0;
     }
 
-    //todo: Implement method and remove this line
+
     private static int multiply(int a, int b) {
-        return 0;
+        return a * b;
     }
 }
